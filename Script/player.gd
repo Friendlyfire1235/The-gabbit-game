b088364cfcd0c66f75d7507699a45784b1243743
Friendlyfire1235 <friendlyfire1928@gmail.com>
2025-10-08 16:06:04 +1300

    The game

--- Script/player.gd
+++ Script/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 const SPEED = 120
 const JUMP_VELOCITY = -400.0
 
@@ -15,16 +17,29 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle movement/deceleration.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("Move_left", "Move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	#flip sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0 :
+		animated_sprite.flip_h = true
+	#play animations
+	if direction == 0 :
+		animated_sprite.play("Idle")
+	
+	else:
+		animated_sprite.play("Run")
+	
+	
+	
 	move_and_slide()
 
 # Called by KillZone when the player dies
