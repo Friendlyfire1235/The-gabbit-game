a53fe092873f6beec57871bbd6f2df50699bb514
Friendlyfire1235 <friendlyfire1928@gmail.com>
2025-10-09 20:11:06 +1300

    updated killzones

--- Script/killzone.gd
+++ Script/killzone.gd
@@ -1,1 +1,10 @@
 extends Area2D
+
+func _ready():
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
+func _on_body_entered(body):
+	# Check if the body is the player
+	if body.is_in_group("Player"):
+		# Instantly kill the player
+		body.death()
--- Script/player.gd
+++ Script/player.gd
@@ -1,54 +1,72 @@
 extends CharacterBody2D
+
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var hearts_parent = $HBoxContainer
 
 const SPEED = 140
 const JUMP_VELOCITY = -300.0
 
-var hearts_list : Array[TextureRect]
+var hearts_list: Array = []  # List of heart TextureRects
 var health = 3
+var alive = true
 
 func _ready() -> void:
-	var hearts_parent = $HBoxContainer
 	for child in hearts_parent.get_children():
-		hearts_list.append(child)
+		if child is TextureRect:
+			hearts_list.append(child)
 	print(hearts_list)
+	update_heart_display()
 
+	# Add player to a group so KillZone can detect it
+	add_to_group("Player")
+
+func take_damage():
+	if health > 0:
+		health -= 1
+		update_heart_display()
+	if health <= 0:
+		death()
+
+func update_heart_display():
+	for i in range(hearts_list.size()):
+		hearts_list[i].visible = i < health
+
+func death():
+	if not alive:
+		return  # Prevent multiple deaths
+	print("Player has died")
+	alive = false
+	await get_tree().create_timer(0.5).timeout
+	get_tree().reload_current_scene()
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	if not alive:
+		return
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle movement/deceleration.
 	var direction := Input.get_axis("Move_left", "Move_right")
-	
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
-	
-	
-	if direction:
+
+	if direction != 0:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-		
+
 	if is_on_floor():
 		if direction == 0:
 			animated_sprite.play("Idle")
-		else :
+		else:
 			animated_sprite.play("Run")
 	else:
 		animated_sprite.play("Jump")
-		
-	
 
 	move_and_slide()
-
-# Called by KillZone when the player dies
